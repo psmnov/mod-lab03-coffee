5dc50b8e49adb3848eb6a73b86556f167359e2f3
psmnov <130275227+psmnov@users.noreply.github.com>
2026-04-05 23:25:06 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,31 @@
-#include <iostream>
+//Copyright 2026 petr smirnov
 #include "Automata.h"
-#include <windows.h>
+#include <iostream>
 int main() {
-	Automata automat;
-	//1
-	automat.on();
-	std::cout << automat.getState() << " ";
-	automat.coin(40);
-	std::cout << automat.getState() << " ";
-	std::cout << "Balance: " << automat.getBalance() << "\n";
-	automat.cancel();
-	/*методы описаны  в соотетствии с диаграммой, поэтому для того, чтобы что-то
-	приготовить когда деньги уже есть на балансе, но до этого клиент выбрал отмена, допустимо нулевое пополнение*/
-	//2
-	std::cout << automat.getState() << " ";
-	automat.coin(0);
-	std::cout << automat.getState() << " ";
-	std::cout << "Balance: " << automat.getBalance() << "\n";
-	automat.getExchange();
-	automat.choice(1);
-	//3
-	std::cout << automat.getState() << " ";
-	automat.cancel();
-	automat.coin(45);
-	std::cout << automat.getState() << " ";
-	std::cout << "Balance: " << automat.getBalance() << "\n";
-	automat.choice(1);
-	automat.getExchange();
-
-	return 0;
+    Automata automat;
+    //1
+    automat.on();
+    std::cout << automat.getState() << " ";
+    automat.coin(40);
+    std::cout << automat.getState() << " ";
+    std::cout << "Balance: " << automat.getBalance() << "\n";
+    automat.cancel();
+    /*методы описаны  в соотетствии с диаграммой, поэтому для того, чтобы что-то
+    приготовить когда деньги уже есть на балансе, но до этого клиент выбрал отмена, допустимо нулевое пополнение*/
+    //2
+    std::cout << automat.getState() << " ";
+    automat.coin(0);
+    std::cout << automat.getState() << " ";
+    std::cout << "Balance: " << automat.getBalance() << "\n";
+    automat.getExchange();
+    automat.choice(1);
+    //3
+    std::cout << automat.getState() << " ";
+    automat.cancel();
+    automat.coin(45);
+    std::cout << automat.getState() << " ";
+    std::cout << "Balance: " << automat.getBalance() << "\n";
+    automat.choice(1);
+    automat.getExchange();
+    return 0;
 }

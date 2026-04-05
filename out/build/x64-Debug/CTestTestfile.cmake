c23798c21a0e99708a16e7084dec5f3c89d58fd1
Peter S <peter.smirnowv@gmail.com>
2026-04-05 22:50:06 +0300

    lab3 initial

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/sebas/mod-lab03-coffee
+# Build directory: C:/Users/sebas/mod-lab03-coffee/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("_deps/googletest-build")
+subdirs("src")
+subdirs("test")
---
+++ out/build/x64-Debug/test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/sebas/mod-lab03-coffee/test
+# Build directory: C:/Users/sebas/mod-lab03-coffee/out/build/x64-Debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

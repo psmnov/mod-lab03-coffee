3a2709c6c3c0f20f95f33978a08b957aa156e9d8
psmnov <130275227+psmnov@users.noreply.github.com>
2026-04-05 22:56:56 +0300

    Create Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+enum STATES {
+	OFF,
+	WAIT,
+	ACCEPT,
+	CHECK,
+	COOK
+};
+class Automata {
+
+public:
+	Automata();
+	void on();
+	void off();
+	void coin(double money);
+	std::vector<std::string> getMenu();
+	STATES getState();
+	void choice(int choice);
+	void cancel();
+	double getBalance();
+	double getExchange();
+private:
+	double cash;
+	STATES state;
+	std::vector<std::string> menu;
+	std::vector<double> prices;
+	void cook(int choice);
+	void finish();
+	bool check(int choice);
+
+};
